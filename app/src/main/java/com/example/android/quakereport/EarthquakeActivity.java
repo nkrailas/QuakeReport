9e65cecd5d0c6fd1ad0f8c7d91fb5f6610e43820
Nida-Krailas <nkrailas@gmail.com>
2018-08-29 00:01:06 -0500

    URL building

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,10 +17,12 @@ package com.example.android.quakereport;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,7 +43,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
     // URL for earthquake data from the USGS dataset
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+            "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
     // Constant value for earthquake loader ID.
     private static final int EARTHQUAKE_LOADER_ID = 1;
@@ -121,7 +123,28 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     @Override
     public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
         // Create a new loader for the given URL
-        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // getString retrieves a String value from the preferences. Second parameter is the default.
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+
+        // .parse breaks apart the URI string that's passed into its parameter.
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+
+        // .buildUpon prepares the baseUri that we just parsed so we can add query parameters to it.
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, 'format=geojson'.
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", "time");
+
+        // Return the completed uri 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&
+        //limit=10&minmag=minMagnitude&orderby=time'.
+        return new EarthquakeLoader(this, uriBuilder.toString());
 
     }
 
